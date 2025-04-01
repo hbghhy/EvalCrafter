a1ccbf9137741517d2c5e08168bd0921e5fc45ee
Hanyu Huang <hanyuhuang@microsoft.com>
2025-04-01 14:36:38 +0800

    add  eval_video_by_dimension and fix dover

---
+++ eval_video_by_dimension.sh
@@ -0,0 +1,99 @@
+EC_path=$1 
+dir_videos=$2
+prompt_dir=$3
+eval_result_dir=$4
+dimensions=$5  # New parameter for dimensions, split by ';'
+
+echo "Starting evaluation script..."
+echo "EC_path: $EC_path"
+echo "dir_videos: $dir_videos"
+echo "prompt_dir: $prompt_dir"
+echo "eval_result_dir: $eval_result_dir"
+echo "dimensions: $dimensions"
+
+# Convert dimensions into an array
+IFS=';' read -r -a dimension_array <<< "$dimensions"
+
+# Function to log and execute a command
+run_command() {
+    echo "Running: $*"
+    eval "$@"
+    if [ $? -ne 0 ]; then
+        echo "Error: Command failed - $*"
+        exit 1
+    fi
+}
+
+# # VQA_A and VQA_T
+if [[ " ${dimension_array[@]} " =~ " VQA " ]]; then
+    echo "Running VQA_A and VQA_T..."
+    cd $EC_path
+    cd ./metrics/DOVER
+    run_command python3 evaluate_dover.py --dir_videos $dir_videos --output_path $eval_result_dir
+fi
+
+# CLIP-Score
+if [[ " ${dimension_array[@]} " =~ " CLIP-Score " ]]; then
+    echo "Running CLIP-Score..."
+    cd $EC_path
+    cd ./metrics/Scores_with_CLIP
+    run_command python3 Scores_with_CLIP.py --dir_videos $dir_videos --metric 'clip_score' --output_path $eval_result_dir --prompt_file $prompt_dir
+fi
+
+# Face Consistency
+if [[ " ${dimension_array[@]} " =~ " Face-Consistency " ]]; then
+    echo "Running Face Consistency..."
+    cd $EC_path
+    cd ./metrics/Scores_with_CLIP
+    run_command python3 Scores_with_CLIP.py --dir_videos $dir_videos --metric 'face_consistency_score' --output_path $eval_result_dir --prompt_file $prompt_dir
+fi
+
+# SD-Score
+if [[ " ${dimension_array[@]} " =~ " SD-Score " ]]; then
+    echo "Running SD-Score..."
+    cd $EC_path
+    cd ./metrics/Scores_with_CLIP
+    run_command python3 Scores_with_CLIP.py --dir_videos $dir_videos --metric 'sd_score' --output_path $eval_result_dir --prompt_file $prompt_dir
+fi
+
+# BLIP-BLUE
+if [[ " ${dimension_array[@]} " =~ " BLIP-BLUE " ]]; then
+    echo "Running BLIP-BLUE..."
+    cd $EC_path
+    cd ./metrics/Scores_with_CLIP
+    run_command python3 Scores_with_CLIP.py --dir_videos $dir_videos --metric 'blip_bleu' --output_path $eval_result_dir --prompt_file $prompt_dir
+fi
+
+# CLIP-Temp
+if [[ " ${dimension_array[@]} " =~ " CLIP-Temp " ]]; then
+    echo "Running CLIP-Temp..."
+    cd $EC_path
+    cd ./metrics/Scores_with_CLIP
+    run_command python3 Scores_with_CLIP.py --dir_videos $dir_videos --metric 'clip_temp_score' --output_path $eval_result_dir --prompt_file $prompt_dir
+fi
+
+# Action Score
+if [[ " ${dimension_array[@]} " =~ " Action-Score " ]]; then
+    echo "Running Action Score..."
+    cd $EC_path
+    cd ./metrics/mmaction2/demo
+    run_command python3 action_score.py --dir_videos $dir_videos --metric 'action_score' --output_path $eval_result_dir
+fi
+
+# Flow-Score
+if [[ " ${dimension_array[@]} " =~ " Flow-Score " ]]; then
+    echo "Running Flow-Score..."
+    cd $EC_path
+    cd ./metrics/RAFT
+    run_command python3 optical_flow_scores.py --dir_videos $dir_videos --metric 'flow_score' --output_path $eval_result_dir
+fi
+
+# Warping Error
+if [[ " ${dimension_array[@]} " =~ " Warping-Error " ]]; then
+    echo "Running Warping Error..."
+    cd $EC_path
+    cd ./metrics/RAFT
+    run_command python3 optical_flow_scores.py --dir_videos $dir_videos --metric 'warping_error' --output_path $eval_result_dir
+fi
+
+echo "Evaluation script completed successfully."
---
+++ metrics/DOVER/evaluate_dover.py
@@ -0,0 +1,145 @@
+import torch
+
+import argparse
+import os
+import pickle as pkl
+
+import decord
+import numpy as np
+import yaml
+from tqdm import tqdm
+import ipdb
+import pandas as pd  # Add this import
+
+
+from dover.datasets import (
+    UnifiedFrameSampler,
+    ViewDecompositionDataset,
+    spatial_temporal_view_decomposition,
+)
+from dover.models import DOVER
+
+mean, std = (
+    torch.FloatTensor([123.675, 116.28, 103.53]),
+    torch.FloatTensor([58.395, 57.12, 57.375]),
+)
+
+aesthetic_sum = 0
+technical_sum = 0
+overall_sum = 0
+
+
+def fuse_results(results: list):
+    ## results[0]: aesthetic, results[1]: technical
+    ## thank @dknyxh for raising the issue
+    # t, a = (results[1] - 0.1107) / 0.07355, (results[0] + 0.08285) / 0.03774
+
+    # use mean and std from generated videos' (prompt700-release) results
+    t, a = (results[1] + 0.0758) / 0.0129, (results[0] - 0.1253) / 0.0318
+    # t, a = results[1], results[0]
+    x = t * 0.6104 + a * 0.3896
+    return {
+        "aesthetic": 1 / (1 + np.exp(-a)),
+        "technical": 1 / (1 + np.exp(-t)),
+        "overall": 1 / (1 + np.exp(-x)),
+    }
+
+def get_quality_score(aesthetic_score, technical_score):
+    quality_weights = np.array([0.03004555, 0.02887537])*5
+    quality_intercept = 0.08707462696457707*5
+    quality = aesthetic_score * quality_weights[0]/ 100 + technical_score * quality_weights[1]/ 100 + quality_intercept
+    return quality * 100
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-o", "--opt", type=str, default="./dover.yml", help="the option file")
+    parser.add_argument("--dir_videos", type=str, default='', help="Specify the path of generated videos")    
+    parser.add_argument("-d", "--device", type=str, default="cuda", help="the running device")
+    parser.add_argument("--output_path", type=str, default='../../results/', help="output_path")
+    args = parser.parse_args()
+    print(args)
+
+    dir_videos = args.dir_videos
+
+    out_path = args.output_path
+    
+    with open(args.opt, "r") as f:
+        opt = yaml.safe_load(f)
+
+    ### Load DOVER
+    evaluator = DOVER(**opt["model"]["args"]).to(args.device)
+    evaluator.load_state_dict(
+        torch.load(opt["test_load_path"], map_location=args.device)
+    )
+
+    video_paths = []
+    all_results = {}
+
+
+    results_list = []
+    dopt = opt["data"]["val-l1080p"]["args"]
+
+    dopt["anno_file"] = None
+    dopt["data_prefix"] = dir_videos
+
+    dataset = ViewDecompositionDataset(dopt)
+
+    dataloader = torch.utils.data.DataLoader(
+        dataset, batch_size=1, num_workers=opt["num_workers"], pin_memory=True,
+    )
+
+    try:
+        with open(
+            f"dover_predictions/val-custom_{dir_videos.split('/')[-1]}.pkl",
+            "rb",
+        ) as rf:
+            all_results = pkl.dump(all_results, rf)
+        print(f"Starting from {len(all_results)}.")
+    except:
+        print("Starting over.")
+
+    sample_types = ["aesthetic", "technical"]
+
+    for i, data in enumerate(tqdm(dataloader, desc="Testing")):
+        if len(data.keys()) == 1:
+            ##  failed data
+            continue
+
+        video = {}
+        for key in sample_types:
+            if key in data:
+                video[key] = data[key].to(args.device)
+                b, c, t, h, w = video[key].shape
+                video[key] = (
+                    video[key]
+                    .reshape(
+                        b, c, data["num_clips"][key], t // data["num_clips"][key], h, w
+                    )
+                    .permute(0, 2, 1, 3, 4, 5)
+                    .reshape(
+                        b * data["num_clips"][key], c, t // data["num_clips"][key], h, w
+                    )
+                )
+
+        with torch.no_grad():
+            results = evaluator(video, reduce_scores=False)
+            results = [np.mean(l.cpu().numpy()) for l in results]
+
+        rescaled_results = fuse_results(results)
+
+        file_name = os.path.basename(data["name"][0])
+        video_gen_id = os.path.splitext(file_name)[0]  # Extract video_gen_id (file name without extension)
+
+        results_list.append({
+            "video_gen_id": video_gen_id,
+            "aesthetic_score": rescaled_results["aesthetic"] * 100,
+            "technical_score": rescaled_results["technical"] * 100,
+            "dover_overall_score": rescaled_results["overall"] * 100,
+            'quality_score': get_quality_score(rescaled_results["aesthetic"] * 100, rescaled_results["technical"] * 100),
+        })
+
+
+    df = pd.DataFrame(results_list)
+    df.to_csv(out_path, sep='\t', index=False)
